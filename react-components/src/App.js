581813c712a8013b84dc6f1b88e2b173ef65b1f4
stephan <smuts1989@gmail.com>
2023-03-08 18:33:57 +0100

    Added a React App

---
+++ react-components/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import { AwesomeButton } from 'react-awesome-button';
+import 'react-awesome-button/dist/styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <AwesomeButton type="primary">Button</AwesomeButton>
+      <AwesomeButton type="secondary">Button</AwesomeButton>
+      <AwesomeButton type="anchor">Button</AwesomeButton>
+      <AwesomeButton type="disabled">Button</AwesomeButton>
+      <AwesomeButton type="progress">Button</AwesomeButton>
+      <AwesomeButton type="primary">Button</AwesomeButton>
+      <AwesomeButton type="primary">Button</AwesomeButton>
+      <AwesomeButton type="primary">Button</AwesomeButton>
+    </div>
+  );
+}
+
+export default App;
